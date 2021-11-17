7c969d139c1e632c5dfd6f898093d102a10adab4
fruitymario <66334659+aandersen1999@users.noreply.github.com>
2021-11-17 16:14:35 -0500

    Version 1

---
+++ CommissionCalculator/CommissionCalculator/CommissionItem.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommissionCalculator
+{
+    public class CommissionItem
+    {
+        static short availableID = 1;
+
+        public short ID { get; }
+        public float Price { get; set; }
+        public bool Replacement { get; set; }
+        public bool OutOfDepartment { get; set; }
+        public bool Return { get; set; }
+        public float ReplacementPrice { get; private set; }
+        public float Commission { get; private set; }
+
+        readonly float under10 = .06f;
+        readonly float between10100 = .03f;
+        readonly float above100 = .015f;
+        readonly float OOD = .01f;
+
+        public CommissionItem(float _price, bool _ood, bool _replacement, bool _return)
+        {
+            Price = _price;
+            OutOfDepartment = _ood;
+            Replacement = _replacement;
+            Return = _return;
+            ID = availableID;
+            availableID++;
+
+            Commission = CommissionReturn();
+            ReplacementPrice = (Replacement) ? ReplacementPriceReturn() : 0;
+
+            Price = (!Return) ? Price : -Price;
+        }
+
+        private float CommissionReturn()
+        {
+            float returnCommission;
+
+            if (!OutOfDepartment)
+            {
+                if (Price < 10) { returnCommission = Price * under10; }
+                else if (Price < 100) { returnCommission = Price * between10100; }
+                else { returnCommission = Price * above100; }
+            }
+            else { returnCommission = Price * OOD; }
+
+            return (!Return) ? returnCommission : -returnCommission;
+        }
+
+        private float ReplacementPriceReturn()
+        {
+            //There's definietly a better way to do this,
+            //but this'll have to do.
+            float returnFloat;
+
+            if (Price < 10) { returnFloat = 1; }
+            else if (Price < 20) { returnFloat = 2; }
+            else if (Price < 25) { returnFloat = 2.5f; }
+            else if (Price < 50) { returnFloat = 5; }
+            else if (Price < 70) { returnFloat = 7; }
+            else if (Price < 100) { returnFloat = 10; }
+            else if (Price < 200) { returnFloat = 20; }
+            else if (Price < 300) { returnFloat = 30; }
+            else if (Price < 400) { returnFloat = 50; }
+            else { returnFloat = 70; }
+
+            return (!Return) ? returnFloat : -returnFloat;
+        }
+    }
+}
--- CommissionCalculator/CommissionCalculator/MainWindow.xaml.cs
+++ CommissionCalculator/CommissionCalculator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,73 @@ using System.Windows.Shapes;
 
 namespace CommissionCalculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<CommissionItem> items = new ObservableCollection<CommissionItem>();
+
+        public float TotalSold { get; private set; }
+        public float TotalCommission { get; private set; }
+        public float TotalRP { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            CommissionList.ItemsSource = items;
+
+            CalculateAll();
+            Update();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float price = float.Parse(ItemPrice.Text);
+                CommissionItem newItem = new CommissionItem(price, (bool)OodBox.IsChecked, (bool)RpBox.IsChecked, (bool)ReturnBox.IsChecked);
+
+                items.Insert(0, newItem);
+                AddNewItem(newItem);
+
+                ItemPrice.Text = string.Empty;
+                OodBox.IsChecked = false;
+                RpBox.IsChecked = false;
+                ReturnBox.IsChecked = false;
+                ErrorMessage.Visibility = Visibility.Hidden;
+
+                Update();
+            }
+            catch (FormatException)
+            {
+                ErrorMessage.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void CalculateAll()
+        {
+            TotalSold = 0;
+            TotalCommission = 0;
+            TotalRP = 0;
+
+            foreach (CommissionItem i in items)
+            {
+                AddNewItem(i);
+            }
+        }
+
+        private void AddNewItem(CommissionItem commissionItem)
+        {
+            TotalSold += commissionItem.Price + commissionItem.ReplacementPrice;
+            TotalCommission += commissionItem.Commission + commissionItem.ReplacementPrice * .1f;
+            TotalRP += commissionItem.ReplacementPrice;
+
+        }
+
+        private void Update()
+        {
+            SoldText.Text = "Total Sold: $" + TotalSold;
+            CommText.Text = "Commission Earned: $" + TotalCommission;
+            PlanText.Text = "From Plans: $" + TotalRP;
         }
     }
 }
